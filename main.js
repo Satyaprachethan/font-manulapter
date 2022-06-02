cd0ef9c044ead12ced8783e0fb02a4363dd3e992
Satyaprachethan <91787398+Satyaprachethan@users.noreply.github.com>
2022-06-02 12:31:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+noseX = 0;
+noseY =0;
+difference = 0;
+rightWristX = 0;
+leftWristX = 0;
+
+function setup() {
+    video = createCapture(VIDEO);
+    video.size(550,500);
+    canvas = createCanvas(550,500)
+    canvas.position(560,150);
+    poseNet = ml5.poseNet(video,modelLoaded)
+    poseNet.on('pose', gotPoses);
+}
+function draw() {
+    document.getElementById("square_sides").innerHTML = "width and height of a text will be " + difference +"px";
+    background(' #ffcccb');
+    fill("white");
+    stroke("black");
+    textSize(difference)
+    text("HELLO",noseX,noseY);
+}
+function modelLoaded() {
+    console.log('poseNet Is Initialized')
+}
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log(noseX, noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        difference = floor(leftWristX - rightWristX);
+        console.log("left Wrist X = " + leftWristX + "right Wrist x = " + rightWristX + "difference = " + difference);
+    }  
+}
